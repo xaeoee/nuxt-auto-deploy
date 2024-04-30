cdec622821be324ad44db99314cb6526befd9eaf
xaeoee <jaeykim3101@gmail.com>
2024-04-30 01:28:10 +0100

    Update Tutorial.vue

--- components/Tutorial.vue
+++ components/Tutorial.vue
@@ -8,7 +8,7 @@
       </a>
       <div class="mt-8 bg-white overflow-hidden shadow sm:rounded-lg p-6">
         <h2 class="text-2xl leading-7 font-semibold">
-          Heloo! Welcome to your Nuxt Application
+          Welcome to your Nuxt Application
         </h2>
       </div>
     </div>
